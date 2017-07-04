1e55f4e0979adf6a68f9adb73dd61e740b28074f
SWLivingstone <scottwlivingstone@gmail.com>
2017-07-03 22:58:19 -0700

    Checkpoint 2
    Configure ActionMailer::Base

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
-    port:           '2525',
-    authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'heroku.com',
-    enable_starttls_auto: true
+       :address => "smtp.gmail.com",
+       :port => 587,
+       :domain => "gmail.com",
+       :user_name => ENV['EMAIL'],
+       :password => ENV['PWORD'],
+       :authentication => :plain,
+       :enable_starttls_auto => true
   }
 end
