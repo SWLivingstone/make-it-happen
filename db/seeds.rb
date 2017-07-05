550256a18ed550d452d8aa2f727739ae69f1cb48
SWLivingstone <scottwlivingstone@gmail.com>
2017-07-05 16:53:29 -0700

    Checkpoint 6
    Create seeded database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!(
+    email: "#{RandomData.random_word}@#{RandomData.random_word}.com",
+    password: "password"
+  )
+end
+
+50.times do
+  Item.create!(
+    user: User.all.sample,
+    name: "#{RandomData.random_sentence}"
+  )
+end
+
+puts "Seed Finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
